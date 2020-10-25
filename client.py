fe8c06bc41a57f2ff7f92e666335f8fa94008c3b
dgarciag1 <47048998+dgarciag1@users.noreply.github.com>
2020-10-24 21:49:42 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,38 +1,81 @@
-#!/usr/bin/env python
+#Client code
 
+#Import socket library
 import socket
+import os
+import constants
+import ast
+#Variables for connection
+host = constants.SERVER_ADDRESS 
+port = constants.PORT
 
-host = '127.0.0.1'
-port = 8050
+def runClientSocket():
+    #Create client socket
+    clientSocket= socket.socket()
 
-socket = socket.socket()
+    #Connection with server. first parameter = IP address, second parameter = Connection Port
+    clientSocket.connect((host, port))
+    print("Connected to server")
 
-socket.connect((host, port))
-print('Conecting...')
 
-while True:
-#Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
-	msg = 'NEW,hola.txt,100'
-#Con el método send, enviamos el mensaje
-	socket.send(msg.encode('ascii'))
-#obj.send(mens)
-	response = socket.recv(1024)
-	print(response)
-	if response.decode('ascii') == 'ACK':
-		f = open('hola.txt', "rb")
-		while True:
-			bytes_read = f.read(1024)
-			if not bytes_read:
-				break
-			print('send')
-			socket.sendall(bytes_read)
-		while True:
-			response = socket.recv(1024)
-			if response.decode('ascii') == 'OK':
-				socket.close()
-				print('closed')
-				break
-	
-#Cerramos la instancia del objeto servidor
+    #Maintain connection with the server
+    while True:
+        #Client command 
+        command = input("Enter the command you want to send >> ")
+        if(command == constants.HELLO or command == constants.LIST_BUCKET):
+            #Send message
+            clientSocket.send(bytes(command, constants.ENCODING_FORMAT))
+            #Server response
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.NEW_BUCKET):
+            bucketName = input("Enter the name of the new bucket >> ")
+            informationToSend = command+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.DELETE_BUCKET):
+            bucketName = input("Enter the name of the bucket to delete >> ")
+            informationToSend = command+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.UPLOAD_FILE):
+            filePath = input("Enter the path where the file is >> ")
+            fileName = input("Enter the name of the file to upload >> ")
+            bucketName = input("Enter the name of the bucket where the file will be uploaded >> ")
+            informationToSend = command+","+filePath+","+fileName+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.LIST_FILE):
+            bucketName = input("Enter the bucket name to list the files >> ")
+            informationToSend = command+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.DOWNLOAD_FILE):
+            fileName = input("Enter the name of the file to download >> ")
+            bucketName = input("Enter the bucket name to download the file >> ")
+            informationToSend = command+","+fileName+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.DELETE_FILE):
+            fileName = input("Enter the name of the file to delete >> ")
+            bucketName = input("Enter the bucket name to delete the file >> ")
+            informationToSend = command+","+fileName+","+bucketName
+            clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+            response = clientSocket.recv(constants.BUFFER_SIZE)
+            print(response.decode())
+        elif(command == constants.EXIT):
+            break
+        else:
+            print("Non-existent command, try again")
+        
+    #Close connection to the server
+    clientSocket.close()
+    print("Connection closed")
 
-#Imprimimos la palabra Adios para cuando se cierre la conexion
+#Iniatialize client socket
+runClientSocket()
